7a13fef2eb76316d6d88113ed3499950f72b1adc
Austin Liu <austin362667@gmail.com>
2021-03-29 17:37:10 +0800

    add pod

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,10 +1,15 @@
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
+
 @import UIKit;
 @import Firebase;
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
-    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [FIRApp configure];
-  return YES;
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+[FIRApp configure];
+return YES;
 }
+
+@end
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,15 +1,15 @@
 import UIKit
+import Flutter
 import Firebase
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-  var window: UIWindow?
-
-  func application(_ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions:
-      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
     FirebaseApp.configure()
-    return true
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
